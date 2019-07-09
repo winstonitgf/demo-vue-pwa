c8024c8dec17e9569b1af9bad9389cf4e6e7269b
Winston 盧文松 <aa71435723@gmail.com>
2019-07-09 09:31:57 +0800

    deploy

---
+++ precache-manifest.cb4179c09cdb57cdd6756f15bf144131.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8b1543ce44b9abab770b",
+    "url": "/js/chunk-vendors.071d7b7a.js"
+  },
+  {
+    "revision": "8a40f8ff16eac62840bf",
+    "url": "/js/app.34d74d56.js"
+  },
+  {
+    "revision": "0317f3a4bfb5daa14f60",
+    "url": "/js/about.13887557.js"
+  },
+  {
+    "revision": "27c67d83101b58016356c28566df60fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1543ce44b9abab770b",
+    "url": "/css/chunk-vendors.93aaabb8.css"
+  },
+  {
+    "revision": "8a40f8ff16eac62840bf",
+    "url": "/css/app.7e7cfee3.css"
+  }
+];
